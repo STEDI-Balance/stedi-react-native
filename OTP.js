26bdb2ae635e63ac891bb9311472f1fb6f224b83
Tyler242 <77693574+Tyler242@users.noreply.github.com>
2021-12-07 16:19:30 -0700

    updated phone number passing to button

--- OTP.js
+++ OTP.js
@@ -1,19 +1,14 @@
-import React from "react";
+import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-
-function OTP() {
-    return(
-      <View>
-      <Button
-      title="Send Code"
-      onPress={() => console.log('OTP Button pressed')}
-    />
-      </View>
-    )
-  }
-
+function OTP(props) {
+  return (
+    <View>
+      <Button title='Send Code' onPress={() => console.log(props.number)} />
+    </View>
+  );
+}
 
 export default OTP;
