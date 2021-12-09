36ff9ab4a849d7343e169876691a149ef1162e93
Junwoo9025 <91073913+Junwoo9025@users.noreply.github.com>
2021-12-09 15:37:32 -0700

    hi

--- OTP.js
+++ OTP.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Card, ListItem, Button, Icon } from 'react-native-elements';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Card, ListItem, Button, Icon } from "react-native-elements";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 function OTP(props) {
   return (
     <View>
       <Button
-        title='Send Code'
+        title="Send Code"
         onPress={() => {
           let number = props.number;
-          console.log(number);
-          fetch('https://dev.stedi.me/twofactorlogin/' + number, {
-            method: 'POST',
+          fetch("https://dev.stedi.me/twofactorlogin/" + number, {
+            method: "POST",
             headers: {
-              Accept: 'application/text',
-              'Content-Type': 'application/text',
+              Accept: "application/text",
+              "Content-Type": "application/text",
             },
             body: JSON.stringify({ number }),
           });
