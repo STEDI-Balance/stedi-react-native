fa8f5e597054a7e7d46b378e71267ffaa5067bc5
Jake Rammell <ram19018@byui.edu>
2022-02-17 16:22:18 -0700

    login button somewhat works

--- Setting.js
+++ Setting.js
@@ -1,17 +1,45 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Counter from './login.js';
+
+const Stack = createNativeStackNavigator();
+
+// const MyStack = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           name="Home"
+//           component={HomeScreen}
+//           options={{ title: 'Welcome' }}
+//         />
+//         <Stack.Screen name="Profile" component={ProfileScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
 
 function Setting() {
     return(
-      <View>
-      <Button
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+          name="Login"
+          component= {Counter}
+          />
+      {/* <Button
       title="Log In"
       onPress={() => Alert.alert('Simple Button pressed')}
-    />
-      </View>
+      onPress={() => 
+        this.navigate(".login.js")}
+    /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
     )
   }
 
