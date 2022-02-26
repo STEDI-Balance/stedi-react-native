d4d0c4ab04082ad6fa591b99e4c2836b68197b6f
Jake Rammell <ram19018@byui.edu>
2022-02-26 12:57:51 -0700

    Login page is now the default if not logged in

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Settings } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Counter from './Counter.js';
@@ -8,11 +8,16 @@ import Home from './Home.js';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Login from "./Login"
 
 // import Icons from "./Icons";
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
+
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  if(userLoggedIn){
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -53,10 +58,11 @@ export default function App() {
       </Tab.Navigator>
     </NavigationContainer>
   );
-
-
+        }
+        else{
+          return (<Login/>)
+        }
 }
-
 
 const styles = StyleSheet.create({
   
--- Setting.js
+++ Setting.js
@@ -3,43 +3,15 @@ import { StyleSheet, Text, View, Image, Alert } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Counter from './login.js';
-
-const Stack = createNativeStackNavigator();
-
-// const MyStack = () => {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen
-//           name="Home"
-//           component={HomeScreen}
-//           options={{ title: 'Welcome' }}
-//         />
-//         <Stack.Screen name="Profile" component={ProfileScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
 
 function Setting() {
     return(
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-          name="Login"
-          component= {Counter}
-          />
-      {/* <Button
+      <View>
+      <Button
       title="Log In"
       onPress={() => Alert.alert('Simple Button pressed')}
-      onPress={() => 
-        this.navigate(".login.js")}
-    /> */}
-        </Stack.Navigator>
-      </NavigationContainer>
+    />
+      </View>
     )
   }
 
